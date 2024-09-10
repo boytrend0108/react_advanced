1ed6aad9065f86f09716835beeade5befcfabe14
boytrend0108 <boytrend@gmail.com>
2024-09-10 09:36:17 +0300

    fix theme provider

--- src/app/providers/ThemeProvider/ui/themeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/themeProvider.tsx
@@ -1,5 +1,4 @@
 import { useMemo, useState } from 'react';
-
 import {
   LOCAL_STORAGE_THEME_KEY,
   Theme,
@@ -9,6 +8,7 @@ import {
 type Props = {
   children: React.ReactNode;
 };
+
 const defaultTheme =
   (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
 
