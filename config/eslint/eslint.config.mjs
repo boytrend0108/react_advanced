b097245566f8d9f669de14df1969a22868e5392d
boytrend0108 <boytrend@gmail.com>
2024-10-04 07:46:28 +0300

    chore: updated eslin config

--- config/eslint/eslint.config.mjs
+++ config/eslint/eslint.config.mjs
@@ -19,6 +19,7 @@ export default [
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
+      'react/display-name': 'off',
     },
   },
 
