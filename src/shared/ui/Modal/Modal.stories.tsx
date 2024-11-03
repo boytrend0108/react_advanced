ea0dc10f3ed07ef025ca7c7818b48939a9d8a585
boytrend0108 <boytrend@gmail.com>
2024-11-03 14:10:04 +0200

    feat: added a modal window for auth with stories

---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Modal } from './Modal';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider2/lib/themeContext';
+
+const meta = {
+  title: 'shared/Modal',
+  component: Modal,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {
+    children: 'Lorem adfasd dfsdftn sdfkldf ',
+    isOpen: true,
+    onClose: () => {},
+  },
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  decorators: [ThemeDecorator(Theme.LIGHT)],
+};
+
+export const Dark: Story = {
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
